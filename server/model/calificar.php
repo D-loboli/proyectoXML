ebded0baac708263e71160062646d86732d0e6f2
PatoAndr33s <patricio.gonzalez.camilo@gmail.com>
2016-06-14 12:19:38 -0300

    calificar

--- server/model/calificar.php
+++ server/model/calificar.php
@@ -7,7 +7,9 @@ $password = $_GET['p'];
 $id_post = $_GET['id'];
 $calificacion = $_GET['calificar=1'];
 
-$d =
+$d = new data();
+
+$calificar =
 if(isset($_POST["btnCalificar"])){
   $calificar=$_POST["txtCalificar"];
 
