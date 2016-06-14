a2dcdf9ea18ce0dba3736981376ae5cb52bda67d
PatoAndr33s <patricio.gonzalez.camilo@gmail.com>
2016-06-14 11:39:09 -0300

    calificar

--- server/model/Data.php
+++ server/model/Data.php
@@ -25,5 +25,19 @@ class Data{
 
         return $res;
     }
+
+    public function calificarPost($usuario, $password, $id_post){
+      $q="insert into evento values
+       (null,
+       '$nombre_evento',
+       '$lugar',
+       '$sede',
+       '$hora_inicio',
+       '$hora_fin',
+       $n_entrada)";
+       $this->c->ejecutar($q);
+    }
+
+
 }
  ?>
--- server/model/calificar.php
+++ server/model/calificar.php
@@ -1,9 +1,14 @@
 <?php
 
+require_once 'Data.php';
+
 $usuario = $_GET['u'];
 $password = $_GET['p'];
 $id_post = $_GET['id'];
 $calificacion = $_GET['calificar=1'];
 
-
+$d =
+if(isset($_POST["btnCalificar"])){
+  $calificar=$_POST["txtCalificar"];
+}
 ?>
