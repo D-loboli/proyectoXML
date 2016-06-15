14cb8f3732abc0fed7ea97e9cfe52bfb9a54f2b1
youkergamer <youker.gam3r@gmail.com>
2016-06-15 10:49:44 -0300

    asjdjkas

---
+++ server/model/eliminar.php
@@ -0,0 +1,20 @@
+html
+
+<?php
+    $usuario = $_GET['u'];
+    $password = $_GET['p'];
+    $id = $_GET['id'];
+    $confirmacion = $_GET['c'];
+    $permiso = $_gET[]
+
+    public function eliminarUsuario ($id,$permiso){
+      if($permiso == 1){
+      $query = "delete from usuario where id = $id";
+      $this->c->ejecutar($query);
+    }
+ }
+
+
+
+
+ ?>
