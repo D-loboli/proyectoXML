c8aa588ea4f7b4aa8384cf2f3a0f8bc276c6586f
youkergamer <youker.gam3r@gmail.com>
2016-06-15 12:01:44 -0300

    hola

--- server/model/eliminar.php
+++ server/model/eliminar.php
@@ -1,15 +1,34 @@
 html
 
 <?php
-    require_once "/Data.php"
-    $usuario = $_GET['u'];
-    $password = $_GET['p'];
-    $id = $_GET['id'];
-    $confirmacion = $_GET['c'];
+require_once "/Data.php"
+$usuario = $_GET['u'];
+$password = $_GET['p'];
+$id = $_GET['id'];
+$confirmacion = $_GET['c'];
 
-    $d = new Data();
+$d = new Data();
 
-    $permiso = $d.getPrivilegio($u,$p,$i);
+$permiso = $d.getPrivilegio($u,$p,$i);
+
+if ($permiso == 1) {
+  if ($d->getEliminarPost($id, $u, $p)){
+    echo "<info>";
+    echo "<mensaje>'Post Eliminado con éxito'<mensaje/>";
+    echo "<delete>true<delete/>";
+    echo "<info/>";
+  }
+}else {
+  echo "<info>";
+  echo "<mensaje>'No posee los privilegios necesarios para realizar esta acción'<mensaje/>";
+  echo "<delete>false<delete/>";
+  echo "<info/>";
+
+}
+
+
+
+
 
 
 
