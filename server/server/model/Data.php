0d48ba1543f43de110d47d01cd811c9f4591723c
D-loboli <lobolinkinxxx@gmail.com>
2016-06-21 14:15:54 -0300

    modificacion

---
+++ server/server/model/Data.php
@@ -0,0 +1,58 @@
+<?php
+require_once "../db/Conexion.php";
+
+class Data{
+    private $d;
+
+    public function __construct(){
+        $this->c = new Conexion(
+            "localhost",
+            "grupo",
+            "root",
+            ""
+        );
+    }
+
+
+    public function getPrivilegio($nick, $clave){
+        $query = "select idRol from usuario where nick = '$nick' and clave = '$clave'";
+        $res = $this->c->ejecutar($query);
+        if ($reg = mysql_fetch_array($res)) $res = $reg;
+        else $res = 0;
+        return $res;
+    }
+
+    public function getEliminarPost($id_post, $id){
+      $query = "delete id = $id_post from post where id_usuario = $id";
+      $res = $this->conexion->ejecutar($query);
+      if ($reg = mysql_fetch_array($res)) $res = $reg;
+      else $res = 0;
+      return $res;
+    }
+
+    public function getRegistrar($nick, $pass, $name){
+        $query = "insert into usuario values(null, 2, $nick, $name, $pass)";
+        $this->c->ejecutar($query);
+    }
+
+    public function getId($nick){
+        $query = "select id from usuario where nick = $nick";
+        if ($reg = mysql_fetch_array($this->c->ejecutar($query)))
+            return $reg[0];
+        else return 0;
+    }
+
+    public function getIngresarPost($idUsuario, $titulo, $texto){
+      $query = "insert into from post values($idUsuario, $titulo, $texto, fecha, calificacion)";
+        $this->c->ejecutar($query);
+    }
+
+    public function getListar(){
+      $qyert = "select from * post";
+      $this->c->ejecutar($query);
+    }
+
+}
+
+
+ ?>
---
+++ server/server/model/eliminar.php
@@ -0,0 +1,27 @@
+<?php
+require_once "Data.php";
+$nick = $_GET['nick'];
+$clave = $_GET['clave'];
+$id_post = $_GET['id_post'];
+$id = $_GET['id'];
+
+$d = new Data();
+
+$permiso = $d->getPrivilegio($nick,$clave);
+
+if ($permiso == 1) {
+  if ($d->getEliminarPost($id_post, $id)){
+    echo "<info>";
+    echo "<mensaje>'Post Eliminado con éxito'<mensaje/>";
+    echo "<delete>true<delete/>";
+    echo "<info/>";
+  }
+}else {
+  echo "<info>";
+  echo "<mensaje>'No posee los privilegios necesarios para realizar esta acción'<mensaje/>";
+  echo "<delete>false<delete/>";
+  echo "<info/>";
+
+}
+
+ ?>
