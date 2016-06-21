d1617090b88a7f81f1f3310774c5f6117a7c5f2b
D-loboli <lobolinkinxxx@gmail.com>
2016-06-21 10:18:20 -0300

    actual de data

---
+++ server/server/model/calificar.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'Data.php';
+
+$usuario = $_GET['u'];
+$password = $_GET['p'];
+$id_post = $_GET['id'];
+$calificacion = $_GET['calificar=1'];
+
+$d = new data();
+if(isset($_POST["btnCalificar"])){
+  $calificar=$_POST["txtCalificar"];
+
+  $calificar = $d.
+}else {
+  echo "<error>Usuario o contraseña incorrectos</error>";
+}
+
+?>
