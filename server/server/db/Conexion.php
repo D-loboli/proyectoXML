8088b81d2918b92498a8819a112bcf792b08e717
PatoAndr33s <patricio.gonzalez.camilo@gmail.com>
2016-06-22 10:12:15 -0300

    actualizacio

--- server/server/db/Conexion.php
+++ server/server/db/Conexion.php
@@ -3,21 +3,22 @@ class Conexion{
     private $con;
 
     public function __construct($server, $bd, $user, $pass){
-        $this->con = mysql_connect($server, $user, $pass);
+        $this->con = mysqli_connect($server, $user, $pass,$bd);
 
         if(!$this->con){
             die("Error al conectar: ".mysql_error());
         }
-
+/*
         $sBD = mysql_select_db($bd, $this->con);
 
         if(!$sBD){
             die("Error al seleccionar: ".mysql_error());
         }
+        */
     }
 
     public function ejecutar($query){
-        return mysql_query($query);
+        return mysqli_query($this->con,$query);
     }
 
 }
--- server/server/model/Data.php
+++ server/server/model/Data.php
@@ -16,17 +16,25 @@ class Data{
 
 
     public function getPrivilegio($nick, $clave){
+      echo "halo";
     $query = "select idRol from usuario where nick = '$nick' and clave = '$clave'";
+    echo "halo2";
     $res = $this->c->ejecutar($query);
-    if ($reg = mysql_fetch_array($res)) $res = $reg;
-    else $res = 0;
+    echo "halo3'.$query'";
+    if ($reg = mysqli_fetch_array($res)){
+      $res = $reg;
+
+    }
+    echo "halo4";
+
+    echo "halo5 '.$query'";
     return $res;
 }
 
     public function getEliminarPost($id_post, $id){
       $query = "delete id = $id_post from post where id_usuario = $id";
       $res = $this->conexion->ejecutar($query);
-      if ($reg = mysql_fetch_array($res)) $res = $reg;
+      if ($reg = mysqli_fetch_array($res)) $res = $reg;
       else $res = 0;
       return $res;
     }
@@ -38,13 +46,13 @@ class Data{
 
     public function getId($nick){
         $query = "select id from usuario where nick = $nick";
-        if ($reg = mysql_fetch_array($this->c->ejecutar($query)))
+        if ($reg = mysqli_fetch_array($this->c->ejecutar($query)))
             return $reg[0];
         else return 0;
     }
 
     public function getIngresarPost($idUsuario, $titulo, $texto){
-      $query = "insert into from post values('$idUsuario', '$titulo', '$texto', '2016-06-18', 4)";
+      $query = "insert into from post values($idUsuario, $titulo, $texto, '2016-06-18', 4)";
         $this->c->ejecutar($query);
     }
 
