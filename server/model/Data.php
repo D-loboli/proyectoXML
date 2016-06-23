14cbef76c29dab4ee311636c012210ffad4617c2
youkergamer <youker.gam3r@gmail.com>
2016-06-23 16:43:09 -0300

    txt

--- server/model/Data.php
+++ server/model/Data.php
@@ -5,56 +5,56 @@ class Data{
   private $c;
 
   public function __construct(){
-      $this->c = new Conexion(
-      "localhost",
-      "grupo",
-      "root",
-      ""
-    );
-  }
+    $this->c = new Conexion(
+    "localhost",
+    "grupo",
+    "root",
+    ""
+  );
+}
 
-  public function isRegistroValido($nick){
+public function isRegistroValido($nick){
 
-    $query = "select count(id) from usuario where nick = '$nick'";
-    $res = $this->c->ejecutar($query);
-    $cantUsers = 0;
+  $query = "select count(id) from usuario where nick = '$nick'";
+  $res = $this->c->ejecutar($query);
+  $cantUsers = 0;
 
-    if ($reg = mysqli_fetch_array($res))
-      $cantUsers = $reg[0];
+  if ($reg = mysqli_fetch_array($res))
+  $cantUsers = $reg[0];
 
-    return $cantUsers == 0;
+  return $cantUsers == 0;
 
-  }
+}
 
-  public function addCalificacion($idPost, $idCalificacion){
-    $query = "update post set calificacion = $calificacion where id = $idPost;";
-    $this->c->ejecutar($query);
+public function addCalificacion($idPost, $idCalificacion){
+  $query = "update post set calificacion = $calificacion where id = $idPost;";
+  $this->c->ejecutar($query);
 
-  }
+}
 
 public function deletePost($id_post){
   $query = "delete from post where id = $id_post";
   $res = $this->c->ejecutar($query);
   /*if ($reg = mysqli_fetch_array($res)) {
-    $res = $reg;
-  }*/
-  return $res;
+  $res = $reg;
+}*/
+return $res;
 }
 
 
 public function getId($nick){
-  	$query = "select id from usuario where nick = '$nick'";
-  	if ($reg = mysqli_fetch_array($this->c->ejecutar($query)))
-  		return $reg[0];
+  $query = "select id from usuario where nick = '$nick'";
+  if ($reg = mysqli_fetch_array($this->c->ejecutar($query)))
+  return $reg[0];
 
-	else return 0;
+  else return 0;
 }
 
 public function getName($idUser){
   $query = "select nombre from usuario where id = $idUser";
 
   if ($reg = mysqli_fetch_array($this->c->ejecutar($query)))
-      return $reg[0];
+  return $reg[0];
 
   else return "nulo";
 
@@ -92,10 +92,25 @@ public function getPrivilegio($nick, $clave){
   return $idPermiso;
 }
 
-  public function addUser($nick, $pass, $name){
-     $query = "insert into usuario values(null, 2, $nick, $name, $pass);";
-     $this->conexion->ejecutar($query);
-  }
+public function addUser($nick, $pass, $name){
+  $query = "insert into usuario values(null, 2, $nick, $name, $pass);";
+  $this->conexion->ejecutar($query);
+}
+
+}
+
+public function getActualizarUsuario($id,$clave){
+  $query="update usuario
+  set clave='$clave'
+  where id='$id'";
+  $this->c->ejecutar($query);
+}
+
+public function getListarUsuario(){
+  $query = "select u.idRol, u.nick, u.nombre from usuario u";
+  $res = $this->c->ejecutar($query);
+}
+}
 }
 
 ?>
